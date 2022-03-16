4ac84b0af9fd4ce89c980ec561c4cdfa811fa992
PC55 <laryea.enl99@gmial.com>
2022-03-16 16:13:21 +0000

    update

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -30,8 +30,8 @@ const article = ({ article }) => {
 // }
 
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-    // const res = await fetch(`${server}/api/articles/${context.params.id}`)
+    // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
     return {
@@ -42,8 +42,8 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-    // const res = await fetch(`${server}/api/articles/`)
+    // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles/`)
 
     const articles = await res.json()
     const ids = articles.map(article => article.id)
