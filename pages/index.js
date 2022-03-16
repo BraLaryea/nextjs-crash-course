be7dd853bcb14de1934aa83b20974144aee74e8d
PC55 <laryea.enl99@gmial.com>
2022-03-16 14:57:51 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
+
+export default function Home({ articles }) {
+  console.log(articles)
+  return (
+    <div >
+      <Head>
+        <title>WebDev News</title>
+        <meta name='keywords' content='web dev' />
+      </Head>
+      <ArticleList articles={articles} />
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
+}
