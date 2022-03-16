9c4193d189ce92b2b435c8b7e36b5149d493da51
PC55 <laryea.enl99@gmial.com>
2022-03-16 16:20:10 +0000

    update

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://mywebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'https://nextjs-crush-course.vercel.app/'
